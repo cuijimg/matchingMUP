966ef347229801c5229119cabb2d1c0c7a8d183a
cuijimg <86956587+cuijimg@users.noreply.github.com>
2021-09-14 17:28:00 +0200

    Add files via upload

--- matchingMUPindustries.py
+++ matchingMUPindustries.py
@@ -7,14 +7,23 @@ Created on Tue Sep 14 15:48:50 2021
 
 import pandas as pd
 from pathlib import Path
+from pandas import DataFrame
 
-file1 = Path(r'I:\!Hiwis\f-cui\mupdata.csv')
+file1 = Path(r'I:\!Hiwis\f-cui\matchingMUP\mupdata.csv')
 file2 = Path (r'I:\!Hiwis\f-kraeme\Internship\readability_sample_withoutnas.csv')
-
+file3 = Path(r'I:\!Hiwis\f-cui\matchingMUP\readability_sample_wonas_wwz.csv.csv')
 df1 = pd.read_csv(file1,usecols=['crefo','wzdig5'], encoding='utf-8')
-print(df1)
+print(df1,type(df1))
+
 df2 = pd.read_csv(file2, encoding='utf-8')
 df2['wz'] = ''
 
-# for i in range(len(df2)):
-#     df2.loc[i,'wz'] = df1.loc
+for index,row1 in df2.iterrows():
+    print(index)
+    for ind,row in df1.iterrows():
+        print(ind)
+        if str(row1['firm identifier'])[:10] == str(row['crefo']):
+            df2.loc[index,'wz'] = row['wzdig5']
+            break
+
+df2.to_csv(file3, encoding = 'utf-8')
