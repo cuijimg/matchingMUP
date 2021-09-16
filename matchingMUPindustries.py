c7f53d84bfa86e95369a24abdd1d3039f06eabe5
cuijimg <86956587+cuijimg@users.noreply.github.com>
2021-09-16 12:48:20 +0200

    improve iter speed

--- matchingMUPindustries.py
+++ matchingMUPindustries.py
@@ -17,13 +17,25 @@ print(df1,type(df1))
 
 df2 = pd.read_csv(file2, encoding='utf-8')
 df2['wz'] = ''
+print(df2)
 
-for index,row1 in df2.iterrows():
+# for index,row1 in df2.iterrows():
+#     print(index)
+#     for ind,row in df1.iterrows():
+#         print(ind)
+#         if str(row1['firm identifier'])[:10] == str(row['crefo']):
+#             df2.loc[index,'wz'] = row['wzdig5']
+#             break
+print(type(df1.loc[2,'crefo']))
+for index,row in df2.iterrows():
     print(index)
-    for ind,row in df1.iterrows():
-        print(ind)
-        if str(row1['firm identifier'])[:10] == str(row['crefo']):
-            df2.loc[index,'wz'] = row['wzdig5']
-            break
+    print(str(row['firm identifier'])[:10])
+    # print(df1['wzdig5'][df1['crefo'] ==int(str(row['firm identifier'])[:10])])
+
+    try:
+        wz = df1['wzdig5'][df1['crefo']==int(str(row['firm identifier'])[:10])].values[0]
+        df2.loc[index,'wz'] = wz
+    except Exception as e:
+        print(index, e)
 
 df2.to_csv(file3, encoding = 'utf-8')
